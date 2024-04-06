85c2d5a83b69a9e06a5674eb4871cbee2de6dcde
Barsan Patricia <barsanpatricia@gmail.com>
2024-04-06 15:13:47 +0300

    Added first draft of game

---
+++ wasteland/Assets/Portal.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Transform transformTp;
+    [SerializeField] GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FPSController player = other.GetComponent<FPSController>();
+        if (player != null)
+        {
+            if (player.justTeleported == false)
+            {
+                StartCoroutine(Teleport(player));
+            }
+            else
+            {
+                player.justTeleported = false;
+            }
+        }
+    }
+    IEnumerator Teleport(FPSController player)
+    {
+        yield return new WaitForSeconds(1);
+        player.justTeleported = true;
+        player.transform.position = new Vector3(
+            transformTp.transform.position.x - 10,
+            transformTp.transform.position.y,
+            transformTp.transform.position.z
+        );
+    }
+}
